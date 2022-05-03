7a95772b46cdc769fb6ba8e53477983fd6a583a6
Breadmania <103375478+Breadmania@users.noreply.github.com>
2022-05-04 01:27:30 +0600

    hw_JS

---
+++ kaka.js
@@ -0,0 +1,32 @@
+// let t1 = 10
+// let t2 = 30
+
+
+// let f33 = (t1 < t2) ?
+//     (tt1, tt2) => console.log('order = ', tt1 + tt2) :
+//     (tt1, tt2) => console.log('order = ', tt1 - tt2) ;
+
+// f33 (t1, t2)
+
+
+
+
+function bratka33(){};
+
+bratka33.prototype.fatpidor = function (){
+    console.log('FATMURLOK !!')
+}
+
+bratka33.prototype.black = function (sun, co2){
+    console.log('MAMBA ==', sun, ' + ', co2)
+}
+
+bratka33.prototype.PLOHOI = function (){
+    console.log('PLOHOI == BRATKA')
+}
+
+let chechnya = new bratka33()
+
+chechnya.fatpidor()
+chechnya.black(4500, 50)
+chechnya.uebok()
